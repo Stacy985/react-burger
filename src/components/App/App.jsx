2c282a3fa47c8b3187ef2b4d26ed910422092f3f
Stacy985 <anastasiya.disy@gmail.com>
2022-11-08 23:53:10 +0300

    дата

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,12 +2,15 @@ import * as React from "react";
 import styles from "../App/App.module.css";
 import AppHeader from "../AppHeader/AppHeader";
 import BurgerConstructor from "../BurgerConstructor/BurgerConstructor";
+import BurgerIngredients from "../BurgerIngredients/BurgerIngredients";
+import { data } from "../../utils/Data.jsx";
 
 const App = () => {
   return (
     <div className={styles.page}>
       <AppHeader />
-      <main>
+      <main className={styles.container}>
+        <BurgerIngredients  />
         <BurgerConstructor />
       </main>
     </div>
--- src/components/BurgerConstructor/BurgerConstructor.tsx
+++ src/components/BurgerConstructor/BurgerConstructor.tsx
@@ -6,23 +6,38 @@ import {
   Button,
 } from "@ya.praktikum/react-developer-burger-ui-components";
 import styles from "../BurgerConstructor/BurgerConstructor.module.css";
+import { ingredientTypes } from "../../utils/Data.jsx";
 
 import PropTypes from "prop-types";
 
 const BurgerConstructor = () => {
   return (
-    <section className={styles.burgerConstrctor}>
-      <div className={styles.container}>
-        <div className="ml-8">
-      {/*     <ConstructorElement
+    <section className={`${styles.burgerConstrctor} pt-25 pr-4 pl-4`}>
+      <div className="ml-8">
+        <div className={styles.container}>
+          <DragIcon type="primary" />
+          <ConstructorElement
             type="top"
             isLocked={true}
-            text={name + "(верх)"}
-            price={6}
-            thumbnail={string.image}
-          /> */}
+            text="Краторная булка N-200i (верх)"
+            price={200}
+            thumbnail={"https://code.s3.yandex.net/react/code/bun-02.png"}
+          />
+          <ul className={'${styles.list} pr-2'}>
+
+          </ul>
+          <ConstructorElement
+            type="bottom"
+            isLocked={true}
+            text="Краторная булка N-200i (низ)"
+            price={200}
+            thumbnail={"https://code.s3.yandex.net/react/code/bun-02.png"}
+          />
         </div>
         <div />
+        <Button htmlType="button" type="primary" size="large">
+          Оформить заказ
+        </Button>
       </div>
     </section>
   );
